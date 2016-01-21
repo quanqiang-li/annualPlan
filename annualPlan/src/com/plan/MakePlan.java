68cf0b1b41c6a6ccb0049480a6043779829c792d
quanqiang-li <2476056494@qq.com>
2016-01-21 15:41:52 +0800

    年度计划表,基本完成,阳历,农历,星期,阳历节日,农历节日

--- annualPlan/src/com/plan/MakePlan.java
+++ annualPlan/src/com/plan/MakePlan.java
@@ -5,30 +5,35 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.ParseException;
 import java.util.Calendar;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.CellStyle;
 
 import com.util.CalendarUtil;
 import com.util.PropertyUtil;
 
 public class MakePlan {
 	
-	public static String[] weeks = {"日", "一", "二", "三", "四", "五", "六" };
 
 	public static void main(String[] args) throws Exception {
 		int yearNum = 2016;// 设置年度年度
 		Calendar instance = Calendar.getInstance();
+		instance.clear();//必须清空,不然会受到时分秒带来的影响
 		instance.set(yearNum, 0, 1);// 月份是从0开始计数
 		InputStream inp = new FileInputStream("年度计划表模版.xls");
 		HSSFWorkbook wb = new HSSFWorkbook(new POIFSFileSystem(inp));
 		HSSFSheet sheet = wb.getSheetAt(0);
-		
+		HSSFCellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setFillPattern(HSSFCellStyle.FINE_DOTS);
+		cellStyle.setFillForegroundColor(HSSFColor.RED.index);//红色背景
+		cellStyle.setFillBackgroundColor(HSSFColor.RED.index);//红色背景
 		int year = yearNum;
 		while (year == yearNum) {
 			int month = instance.get(Calendar.MONTH) + 1;// 因为计算机的月份是从0开始计数的,+1就符合了人类的习惯
@@ -40,7 +45,11 @@ public class MakePlan {
 			//HSSFCell cell = sheet.createRow(date).createCell(month);//这种方式下一个月份的行会把整个行覆盖掉
 			sheet.setColumnWidth(month, 2560);//256表示一个字节的长度
 			cell.setCellValue(getCellValue(instance));
-
+			//周末背景标红
+			String week = CalendarUtil.getWeek(instance);
+			if(week.equals("周六") || week.equals("周日")){
+				cell.setCellStyle(cellStyle);
+			}
 			instance.add(Calendar.DATE, 1);
 			year = instance.get(Calendar.YEAR);
 		}
@@ -59,15 +68,17 @@ public class MakePlan {
 	 * 根据日历获取需要写入模版的单元格内容
 	 * @param instance 日历实例
 	 * @return
+	 * @throws ParseException 
 	 */
-	private static String getCellValue(Calendar instance){
-		int week = instance.get(Calendar.DAY_OF_WEEK);//周日为1,周六为7
-		String weekSty = weeks[week-1];
-		String mmdd = CalendarUtil.getMMDD(instance);
-		System.out.println(mmdd);
+	private static String getCellValue(Calendar instance) throws ParseException{
+		/*农历+阳历节日+农历节日*/
+		String lunarCalendar = CalendarUtil.getLunarCalendar(instance);//农历
 		PropertyUtil propertyUtil = PropertyUtil.getInstance();
-		String solar = propertyUtil.properties.getProperty(mmdd,"");
-		String value = solar.equals("") ? weekSty : weekSty + "|" + solar;
+		String solar = propertyUtil.properties.getProperty(CalendarUtil.getStyleDate(instance, "MMdd"),"");//阳历节日
+		String lunar = propertyUtil.properties.getProperty(lunarCalendar,"");//农历节日
+		//显示的农历只要后两位即可,如果是初一则显示农历月份
+		lunarCalendar = lunarCalendar.substring(2).equals("初一") ? lunarCalendar.substring(0,2) : lunarCalendar.substring(2);
+		String value = lunarCalendar + solar + lunar;
 		System.out.println(value);
 		return value;
 	}
--- annualPlan/src/com/util/CalendarUtil.java
+++ annualPlan/src/com/util/CalendarUtil.java
@@ -1,44 +1,82 @@
 package com.util;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CalendarUtil {
 
 	public static String[] weeks = {"周日", "周一", "周二", "周三", "周四", "周五", "周六" };
+	public static String[] days = {"初一","初二","初三","初四","初五","初六","初七","初八","初九","初十",
+								   "十一","十二","十三","十四","十五","十六","十七","十八","十九","二十",
+								   "廿一","廿二","廿三","廿四","廿五","廿六","廿七","廿八","廿九","三十"};
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Calendar instance = Calendar.getInstance();
-		instance.set(2015, 11, 12);
-		String mmdd = getMMDD(instance);
-		System.out.println(mmdd);
-		System.out.println(instance.getTime().toLocaleString());
+		instance.clear();
+		instance.set(2016, 0, 5);
+		String a = "正月初二";
+		System.out.println(a.substring(2).equals("初一") ? a.substring(0,2) : a.substring(2) );
+		
 	}
 	/**
-	 * 根据日历返回年月
-	 * @param calendar
-	 * @return 月份是1-12 ，如201512
-	 * @author carl918@163.com
+	 * 根据指定样式把日历转换成字符串
+	 * @param calendar 日历实例
+	 * @param style 年用YYYY或者yyyy,月用MM,日用DD或dd,<br>
+	 * 小时HH 24制,hh 12制,分钟mm,秒ss
+	 * @return 
 	 */
-	public static String getMMDD(Calendar calendar){
+	public static String getStyleDate(Calendar calendar,String style){
 		Date date = calendar.getTime();
-		DateFormat df = new SimpleDateFormat("MMdd");
+		DateFormat df = new SimpleDateFormat(style);
 		return df.format(date);
 	}
+	/**
+	 * 根据指定样式把字符串转换成日期
+	 * @param date
+	 * @param style 年用YYYY或者yyyy,月用MM,日用DD或dd,<br>
+	 * 小时HH 24制,hh 12制,分钟mm,秒ss
+	 * @return
+	 * @throws ParseException
+	 */
+	public static Date getStyleDate(String date,String style) throws ParseException{
+		DateFormat df = new SimpleDateFormat(style);
+		return df.parse(date);
+	}
 	
 	/**
-	 * 阳历转换成农历
-	 * @param calendar
-	 * @return 月份从正月到腊月  天数廿代表二十
+	 * 阳历转换成农历<br>
+	 * 给定阳历年份去获取配置文件中录入的所有该年的农历月份初一对应的阳历日,称作节点日<br>
+	 * 找到一个小于等于且最接近给定阳历日的节点日,获取该节点日对应位置的农历月份<br>
+	 * 用给定日-节点日+1得到农历日期
+	 * @param calendar 日历实例
+	 * @return 月份从正月到腊月  天数廿代表二十,如果没有记录,则返回"农历暂缺";占四个汉字位置,never null
 	 * @author carl918@163.com
+	 * @throws ParseException 
 	 */
-	public static String getLunarCalendar(Calendar calendar){
-		
-		return null;
+	public static String getLunarCalendar(Calendar calendar) throws ParseException{
+		String lunar = "农历暂缺";
+		String date = getStyleDate(calendar, "yyyyMMdd");
+		int year = calendar.get(Calendar.YEAR);
+		PropertyUtil propertyUtil = PropertyUtil.getInstance();
+		String keys = propertyUtil.properties.getProperty(year + "key","");
+		String values = propertyUtil.properties.getProperty(year + "value","");
+		String[] nodes = keys.split(",");
+		String[] nodeValues = values.split(",");
+		int index = 0;
+		for(int i = nodes.length-1;i>=0;i--){
+			if(nodes[i].compareTo(date) <= 0){
+				index = i;//月份确认
+				break;
+			}
+		}
+		//获取的long不可强制转int,会得到预料之外的结果,使用intValue
+		Long day = (calendar.getTimeInMillis() - getStyleDate(nodes[index], "yyyyMMdd").getTime())/(24*60*60*1000) + 1;
+		//月份+日子
+		lunar = nodeValues[index] + days[day.intValue()-1];
+		return lunar;
 	}
 	/**
 	 * 阳历转换成星期
