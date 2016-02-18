e593b853cbc18e29bcfb76333b6b1dbb445b035d
quanqiang-li <2476056494@qq.com>
2016-02-18 17:42:06 +0800

    增加天干地支,吉时

--- annualPlan/src/com/util/CalendarUtil.java
+++ annualPlan/src/com/util/CalendarUtil.java
@@ -12,14 +12,34 @@ public class CalendarUtil {
 	public static String[] days = {"初一","初二","初三","初四","初五","初六","初七","初八","初九","初十",
 								   "十一","十二","十三","十四","十五","十六","十七","十八","十九","二十",
 								   "廿一","廿二","廿三","廿四","廿五","廿六","廿七","廿八","廿九","三十"};
+	public static String[] ganzhi = {"甲子", "乙丑", "丙寅", "丁卯", "戊辰", "己巳", "庚午",	"辛未", "壬申", "癸酉", 
+									 "甲戌", "乙亥", "丙子", "丁丑", "戊寅", "己卯", "庚辰", "辛巳", "壬午", "癸未", 
+									 "甲申", "乙酉", "丙戌", "丁亥", "戊子", "己丑", "庚寅", "辛卯", "壬辰", "癸巳", 
+									 "甲午", "乙未", "丙申", "丁酉", "戊戌", "己亥", "庚子", "辛丑", "壬寅", "癸卯",
+									 "甲辰", "乙巳", "丙午", "丁未", "戊申", "己酉", "庚戌", "辛亥", "壬子", "癸丑", 
+									 "甲寅", "乙卯", "丙辰", "丁巳", "戊午", "己未", "庚申", "辛酉", "壬戌", "癸亥" };
+	private static String[][] luckyTime= {{"子","23-3,5-7,11-13,15-19"},{"午","23-3,5-7,11-13,15-19"},
+								   {"丑","3-7,9-11,15-17,19-23"},{"未","3-7,9-11,15-17,19-23"},
+								   {"寅","23-3,7-11,13-15,19-21"},{"申","23-3,7-11,13-15,19-21"},
+								   {"卯","23-1,3-7,11-15,17-19"},{"酉","23-1,3-7,11-15,17-19"},
+								   {"辰","3-5,7-11,15-19,21-23"},{"戌","3-5,7-11,15-19,21-23"},
+								   {"巳","1-3,7-9,11-15,19-23"},{"亥","1-3,7-9,11-15,19-23"}
+									};
 	
+
+
 	public static void main(String[] args) throws ParseException {
+
 		Calendar instance = Calendar.getInstance();
 		instance.clear();
-		instance.set(2016, 0, 5);
-		String a = "正月初二";
-		System.out.println(a.substring(2).equals("初一") ? a.substring(0,2) : a.substring(2) );
-		
+		instance.set(2016, 1, 18);
+		String dayGanzhi = getDayGanzhi(instance);
+		String diZhi = dayGanzhi.substring(1);
+		for(int i = 0;i<luckyTime.length;i++){
+			if(diZhi.equals(luckyTime[i][0])){
+				System.out.println(luckyTime[i][1]);
+			}
+		}
 	}
 	/**
 	 * 根据指定样式把日历转换成字符串
@@ -88,5 +108,25 @@ public class CalendarUtil {
 		int week = calendar.get(Calendar.DAY_OF_WEEK);//周日为1,周六为7
 		String weekSty = weeks[week-1];
 		return weekSty;
+	}
+	/**
+	 * 获取指定阳历日的日干支<br>
+	 * 以20151214作为原点日,此日正好是"甲子",任意甲子日均可<br>
+	 * 用(指定日历-原地日)%60的值,作为干支数组的索引即可
+	 * @param calendar
+	 * @return 日干支,如甲子,乙丑
+	 */
+	public static String getDayGanzhi(Calendar calendar){
+		Calendar instance = Calendar.getInstance();
+		instance.clear();
+		instance.set(2015, 11, 14);//原点日,甲子日
+		//比原点日多几天
+		Long num = (calendar.getTimeInMillis()-instance.getTimeInMillis())/(24*60*60*1000);
+		int value = num.intValue() % 60;
+		//处理负数的情况
+		if(value < 0){
+			value = value + 60;
+		}
+		return ganzhi[value];
 	}
 }
